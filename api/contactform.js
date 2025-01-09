f6db4056d154659875e6c196f8f9666c692de5a2
johannes-code <johannes24sfj@gmail.com>
2025-01-09 10:13:38 +0100

    updated(request)

--- api/contactform.js
+++ api/contactform.js
@@ -3,7 +3,7 @@ import { Resend }  from "resend"
 const apiKey = process.env.RESEND_API_KEY
 const resend = new Resend(apiKey)
 
-export async function POST() {
+export async function POST(request) {
     const data = await request.json();
     console.log(data);
     // resend.emails.send({
