21399e95d960757a37281a366d985a05cfe853c1
johannes-code <johannes24sfj@gmail.com>
2025-01-09 11:30:17 +0100

    updated logic added html

--- api/contactform.js
+++ api/contactform.js
@@ -4,17 +4,19 @@ const apiKey = process.env.RESEND_API_KEY
 const resend = new Resend(apiKey)
 
 export async function POST(request) {
-    const data = await request.json();
-    console.log(data);
-    // resend.emails.send({
-    //     from: "contact@resend.dev",
-    //     to: "johannes24sfj@gmail.com",
-    //     subject: "Contact wanted",
-    //     html: `
-    //         <h1> Message from somebody</h1>
-    //         <p> They said nice things about med</p>  `
+    const contactFormData = await request.json();
+    const result = await resend.emails.send({
+        from: "contact@resend.dev",
+        to: "johannes24sfj@gmail.com",
+        replyTo: contactFormData.email,
+        subject: "ContactForm",
+        html: `
+            <h1> You got attention! </h1>
+            <h2>${contactFormData.name} </h2>
+            <h2>${contactFormData.email} </h2>
+            <p>${contactFormData.message} </p>  `
 
-    // })
+    })
     return new Response ("Hello world!")
 }
 
--- main.js
+++ main.js
@@ -1,15 +1,29 @@
+const formElement = document.querySelector("#contactform");
+    if (formElement === null) {
+        throw Error("Missing the form element 'contactform'");
+    }
 
+
+    formElement.addEventlistener("submit", async (event) => {
+        event.preventDefault();
+
+        //prevents submission from refreshing the page
+    })
+
+//form info    
 const contactMessage = {
-    email: "bjottleif@not.home",
-    message: "not at home",
-
+    name: document.querySelector("#name").value,
+    email: document.querySelector("#email").value,
+    message: document.querySelector("#message").value,
 };
 
+//sends the message to backend
 const response = await fetch("/api/contactform",{
     method: "POST",
     body: JSON.stringify(contactMessage),
-
 });
-const data = await response.text()
 
-console.log(data)
+    if (response.ok) {
+        formELement.reset();
+    }
+;
